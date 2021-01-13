5cf215bd8d052259abcc6d2f164354286db090b3
Maciej Glowacki <m.glowacki01@gmail.com>
2021-01-13 12:28:04 +0100

    Add Welcome comp with responsive design

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -6,13 +6,15 @@ import { Typography } from '@material-ui/core';
 import laptop_working from '../assets/laptop-working.json';
 import Lottie from '../common/Lottie/Lottie';
 import SocialMedia from '../SocialMedia/SocialMedia';
+import { ChevronLeftRounded, ChevronRightRounded } from '@material-ui/icons';
 
 // Styles
 import classes from './Welcome.module.scss';
 
 const useStyles = makeStyles((theme) => ({
-	alignLeft: {
-		textAlign: 'left',
+	h3: {
+		margin: '10px 0',
+		fontWeight: 400,
 	},
 }));
 
@@ -26,10 +28,16 @@ const Welcome = () => {
 			</div>
 			<div className={classes.text_container}>
 				<SocialMedia line />
-				<Typography className={classes_Mui.textAlign} variant="h3">
-					Hello there, I'm Mats.
+				<Typography className={classes_Mui.h3} variant="h3" component="h1">
+					Hi there, I'm Mats.
 				</Typography>
-				<Typography variant="h4">Fullstack JavaScript Developer</Typography>
+				<Typography className={classes_Mui.h3} variant="h3" component="h1">
+					Fullstack JavaScript <span className="contrast">Developer</span>
+				</Typography>
+				<div className={classes.background_chevrons}>
+					<ChevronLeftRounded />
+					<ChevronRightRounded />
+				</div>
 			</div>
 		</div>
 	);
