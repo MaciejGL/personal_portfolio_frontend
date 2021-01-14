ab18c9a34becb119fd41e01acf5c6c423661ab87
Maciej Glowacki <m.glowacki01@gmail.com>
2021-01-14 17:41:05 +0100

    Badge can be loaded from image/png

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -7,16 +7,14 @@ import { Typography } from '@material-ui/core';
 // Styles
 import classes from './Badge.module.scss';
 
-const Badge = ({ badge }) => {
-	return (
-		<div className={classes.badge_container}>
-			<i className={badge.iconClass}></i>
-			<Typography variant="body2" className={classes.badge_name}>
-				{badge.name}
-			</Typography>
-		</div>
-	);
-};
+const Badge = ({ badge }) => (
+	<div className={classes.badge_container}>
+		{badge.iconClass ? <i className={badge.iconClass}></i> : <img className={classes.badge_img} src={badge.img.publicURL} alt={badge.name} />}
+		<Typography variant="body2" className={classes.badge_name}>
+			{badge.name}
+		</Typography>
+	</div>
+);
 
 Badge.propTypes = {
 	badge: PropTypes.object.isRequired,
