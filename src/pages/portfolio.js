a69e7b78de23cd9e988ae37d43c097359282eaf6
Maciej Glowacki <m.glowacki01@gmail.com>
2021-01-23 21:35:55 +0100

    clean unused components and remove headers in axios

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,18 +3,15 @@ import React from 'react';
 // Components
 import Layout from '../components/Layout';
 import Timeline from '../components/Portfolio/Timeline/Timeline';
-import InTouch from '../components/InTouch/InTouch';
 import SEO from '../components/SEO/SEO';
 import GithubCalendar from '../components/GithubCalendar/GithubCalendar';
 
-const portfolio = () => {
-	return (
-		<Layout>
-			<SEO title="Portfolio" article="Portfolio" />
-			<Timeline />
-			<GithubCalendar />
-		</Layout>
-	);
-};
+const portfolio = () => (
+	<Layout>
+		<SEO title="Portfolio" article="Portfolio" />
+		<Timeline />
+		<GithubCalendar />
+	</Layout>
+);
 
 export default portfolio;
